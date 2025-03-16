48945d794c80eb7d2262b377b4cd899a251f485e
Torge Rosendahl <torgeros@tuta.com>
2025-03-16 13:43:02 +0100

    add new "section" component

---
+++ components/Section.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :id="id">
+    <h2>{{ name }}</h2>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    id(): string {
+      return this.name
+        .toLowerCase()
+        .replace(/[\s-]+/g, '-') // Replace spaces (and hyphens) with hyphens
+        .replace(/[^\w-]/g, ''); // Remove special characters
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <h1>Torge Rosendahl</h1>
-    <div>
-      <h2>Socials</h2>
-      <CardCollection>
-        <SocialCard icon="mdi:linkedin" prefix="in/" url="https://linkedin.com/in/torgeros">
-          torgeros
-        </SocialCard>
-        <SocialCard icon="mdi:instagram" url="https://instagram.com/torgeros">
-          torgeros
-        </SocialCard>
-        <SocialCard icon="mdi:mastodon" url="https://norden.social/@torgeros">
-          <!--zero width space for breaking-->
-          torgeros&#8203;@norden.social
-        </SocialCard>
-        <SocialCard icon="mdi:github" url="https://github.com/torgeros">
-          torgeros
-        </SocialCard>
-      </CardCollection>
-    </div>
-  </div>
+  <h1>Torge Rosendahl</h1>
+  <Section name="Socials">
+    <CardCollection>
+      <SocialCard icon="mdi:linkedin" prefix="in/" url="https://linkedin.com/in/torgeros">
+        torgeros
+      </SocialCard>
+      <SocialCard icon="mdi:instagram" url="https://instagram.com/torgeros">
+        torgeros
+      </SocialCard>
+      <SocialCard icon="mdi:mastodon" url="https://norden.social/@torgeros">
+        <!--zero width space for breaking-->
+        torgeros&#8203;@norden.social
+      </SocialCard>
+      <SocialCard icon="mdi:github" url="https://github.com/torgeros">
+        torgeros
+      </SocialCard>
+    </CardCollection>
+  </Section>
 </template>
