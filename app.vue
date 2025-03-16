f7b71abb9f6d4e1e40be9cb311e4426f356d5db7
Torge Rosendahl <torgeros@tuta.com>
2025-03-16 14:45:01 +0100

    remove WIP banner on dev deployment

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <div style="text-align: center;">🚧 WORK IN PROGRESS! 🚧</div>
+    <template v-if="!isDev">
+      <div style="text-align: center;">🚧 WORK IN PROGRESS! 🚧</div>
+    </template>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+const isDev = process.env.NODE_ENV === 'development';
+</script>
