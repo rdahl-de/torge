fad3514b565083889454f58a720003cf00cca4cc
Torge Rosendahl <torgeros@tuta.com>
2025-03-20 03:53:39 +0100

    remove WIP banner on live page

--- app.vue
+++ app.vue
@@ -1,14 +1,7 @@
 <template>
   <div>
-    <template v-if="!isDev">
-      <div style="text-align: center;">🚧 WORK IN PROGRESS! 🚧</div>
-    </template>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
-
-<script setup>
-const isDev = process.env.NODE_ENV === 'development';
-</script>
